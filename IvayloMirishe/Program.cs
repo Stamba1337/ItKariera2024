ae3e5a78a3dfb6f694ce8c595b685294288a5c5c
Stanimir Lambov <stamba1337@gmail.com>
2024-01-06 10:16:01 +0200

    Initial Commit

---
+++ IvayloMirishe/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace IvayloMirishe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ivaylo Mirishe na banani");
+            Console.WriteLine("Darya - Portokal");
+        }
+    }
+}
